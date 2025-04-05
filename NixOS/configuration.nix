cd2998d5902fd0ffa661bfb25ac4e85861aa13b0
Akil <141535206+AnJLFifi@users.noreply.github.com>
2025-04-05 19:02:53 +0000

    Update configuration.nix

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -1,31 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Recife";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "pt_BR.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -38,42 +26,31 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
   services.flatpak.enable = true;
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  xdg.portal.config.common.default = "*";  # Para garantir comportamento antigo
+  xdg.portal.config.common.default = "*";
 
-  # Enable the SDDM display manager
   services.displayManager.sddm.enable = true;
-
-  # Disable Plasma
-  services.desktopManager.plasma6.enable = false;
-
-  # Enable i3wm
-  services.xserver.windowManager.i3.enable = true;
-
-  # Disable Wayland for SDDM (if necessary)
   services.displayManager.sddm.wayland.enable = false;
+
+  services.desktopManager.plasma6.enable = false;
+  services.xserver.windowManager.i3.enable = true;
 
   environment.sessionVariables = {
     XDG_SESSION_TYPE = "x11";
     QT_QPA_PLATFORM = "xcb";
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -83,17 +60,11 @@
     pulse.enable = true;
   };
 
-  # Enable Bluetooth
   hardware.bluetooth.enable = true;
-  services.blueman.enable = true; # Interface gráfica opcional
-  
-  # Se o adaptador precisar de firmware proprietário
+  services.blueman.enable = true;
   hardware.enableAllFirmware = true;
-  
-  # Se for um adaptador USB, garantir que o módulo está carregado
   boot.kernelModules = [ "btusb" ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.anjl = {
     isNormalUser = true;
     description = "anjl";
@@ -103,36 +74,19 @@
     ];
   };
 
-  # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "anjl";
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    pkgs.alacritty
-    pkgs.steam
-    pkgs.discover
-vim
+    alacritty
+    steam
+    discover
+    vim
   ];
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Allow unfree packages (Steam in this case)
   nixpkgs.config.allowUnfree = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken.
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
