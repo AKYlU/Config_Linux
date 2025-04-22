1db6712724102144bf8edcb68341ea457d36ec58
AKYIL <141535206+AKYlU@users.noreply.github.com>
2025-04-22 14:57:27 -0300

    Update configuration.nix

--- NixOS/Wayfire/arquivo/configuration.nix
+++ NixOS/Wayfire/arquivo/configuration.nix
@@ -64,9 +64,9 @@
     gnome.gnome-control-center                # necessário para configurar fontes Flatpak
     gnome.gnome-settings-daemon               # integração de configurações GNOME
     gnome.gnome-keyring                       # chaveiro para autenticação Flatpak
-    gnome.gnome-software-plugin-flatpak       # plugin para suporte ao Flatpak no gnome-software
     networkmanagerapplet
-
+    pavucontrol
+ 
     wayfire
     xwayland
     wlsunset
