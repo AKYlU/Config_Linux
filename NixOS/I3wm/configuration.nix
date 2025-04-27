f7039d5504720cd994e7000a93c9268dcf57d6ed
AKYIL <141535206+AKYlU@users.noreply.github.com>
2025-04-26 22:40:34 -0300

    Update configuration.nix

--- NixOS/I3wm/configuration.nix
+++ NixOS/I3wm/configuration.nix
@@ -9,6 +9,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
+  nixpkgs.config.allowUnfree = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
