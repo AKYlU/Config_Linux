8c4aa1e3b9071fafdb7115023399174f3fb74164
AKYIL <141535206+AKYlU@users.noreply.github.com>
2025-05-18 12:23:53 -0300

    Add files via upload

--- NixOS/Wayfire/Config/configuration.nix
+++ NixOS/Wayfire/Config/configuration.nix
@@ -8,6 +8,18 @@
   boot.loader.systemd-boot.enable = true;                # Habilita o carregador de sistema systemd-boot
   boot.loader.efi.canTouchEfiVariables = true;           # Permite ao NixOS escrever variáveis EFI
 
+  system.autoUpgrade.enable = true;                      # updete
+  system.autoUpgrade.allowReboot = true;
+
+  programs.appimage.enable = true;                       # AppImage
+  programs.appimage.binfmt = true;
+
+  programs.appimage.package = pkgs.appimage-run.override { 
+    extraPkgs = pkgs: [
+      # missing libraries here, e.g.: `pkgs.libepoxy`    # appimage
+    ];
+  };
+
   nixpkgs.config.allowUnfree = true;                     # Permite pacotes com licenças não-livres
 
   networking.hostName = "wayfire-nixos";                 # Define o nome de host da máquina
@@ -15,33 +27,14 @@
   i18n.defaultLocale = "en_US.UTF-8";                    # Define o locale padrão do sistema
   console.keyMap = "us";                                 # Define o layout de teclado do console
 
-  services.flatpak.enable = true;
-
   networking.networkmanager.enable = true;               # Habilita o NetworkManager para gerenciar conexão de rede
 
   users.users.akyil = {                                  # Configuração do usuário 'akyil'
     isNormalUser = true;                                 # Usuário não-root
-    description = "Usuário Akyil";                       # Descrição do usuário
+    uid = 1000;                                          # ID de usuário
     extraGroups = [ "wheel" "video" "audio" "input" "libvirtd" ];  # Grupos adicionais
     shell = pkgs.fish;                                   # Define o shell padrão como Fish
   };
-
-  # [...]
-
-  nixpkgs.overlays = [
-    (import /home/akyil/nixos/config/overlays/pywm-wayland-scanner.nix)  # Caminho para o seu overlay
-  ];
-
-virtualisation.libvirtd = {
-  enable = true;                   # Ativa o libvirtd
-  qemu = {
-    package = pkgs.qemu_kvm;       # Usa QEMU com suporte KVM
-    runAsRoot = false;             # Para uso como usuário normal
-    swtpm.enable = true;           # TPM opcional, seguro deixar
-  };
-};
-
-  programs.nix-ld.enable = true; # ativa o compat de binários externos
 
   programs.fish.enable = true;                           # Habilita o Fish como shell interativo
 
@@ -65,44 +58,31 @@ virtualisation.libvirtd = {
     ];
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  virtualisation.libvirtd.enable = true;                 # Habilita o serviço libvirtd para VMs
 
-  # virtualisation.libvirtd.enable = true;                 # Habilita o serviço libvirtd para VMs
+  networking.networkmanager.insertNameservers = [ "94.140.14.15" "94.140.15.16" ];
 
-  systemd.services."start-libvirt-default-network" = {   # Serviço systemd para iniciar a rede "default" do libvirt no boot
-    description = "Start default libvirt network on boot"; 
+  networking.bridges.br0.interfaces = [ "enp3s0" ];  # aqui sua interface real
+  networking.interfaces.br0.useDHCP = true;
 
-    after = [ "libvirtd.service" ];                      # Só inicia depois que o serviço libvirtd estiver rodando
-    
-    wantedBy = [ "multi-user.target" ];                  # Garante que rode no boot
-    
-    serviceConfig = {
-      Type = "oneshot";                                  # Executa uma vez e termina
-      ExecStart = "/run/current-system/sw/bin/virsh net-start default"; # Comando para iniciar a rede
-      RemainAfterExit = true;                            # Marca como "ativo" mesmo após terminar
-    };
-  };
 
-  # services.spice-vdagentd.enable = true;               # Habilita o agente SPICE para VMs
+  networking.firewall.enable = false;                    # firewall 
 
-networking.firewall.allowedTCPPorts = [
-  8090  # Porta para transmissão de som
-  6969  # Porta para outro serviço (ex: controle remoto)
-  27015
-];
+  # networking.firewall.allowedTCPPorts = [
+  #  8090  # Porta para transmissão de som
+  #  6969  # Porta para outro serviço (ex: controle remoto)
+  #  27015
+  # ];
 
-networking.firewall.allowedUDPPorts = [
-  27015 # Porta UDP exigida pelo jogo
-];
+  # networking.firewall.allowedUDPPorts = [
+  #  27015 # Porta UDP exigida pelo jogo steam
+  # ];
 
 
   # Se UDP for necessário, descomente a linha abaixo
   # networking.firewall.allowedUDPPorts = [ 6969 ];      # Abre porta UDP 6969
 
   environment.systemPackages = with pkgs; [              # Pacotes instalados globalmente
-
-    # estou pesando
-    # android-studio      # IDE Android
 
     blender-hip         # Programa de modelagem 3D especializado
     reaper              # DAW para edição de áudio
@@ -121,6 +101,7 @@ networking.firewall.allowedUDPPorts = [
     ffmpeg              # gravador
     wf-recorder         # gravar video
     swww                # wallpepar
+    spice-vdagent       # habilita copiar/colar
 
     libsForQt5.dolphin  # Dolphin file manager (Qt5)
     libsForQt5.ark      # Ark para compressão/extração (Qt5)
@@ -130,72 +111,31 @@ networking.firewall.allowedUDPPorts = [
     steam               # Plataforma de jogos Steam
     prismlauncher       # Launcher para jogos Prismatik
     jdk21               # Java Development Kit 21
-    git
+    git                 # github
 
     pulseaudio          # Cliente PulseAudio (requerido por alguns apps)
-    pulseaudio-dlna
     playerctl           # Controle de player via CLI
+    pulsemixer          # conltolede som
+    qpwgraph            # para manda som da utau   
 
     libsForQt5.konsole  # Terminal Konsole (Qt5)
     fish                # Fish shell
     fastfetch           # Ferramenta de info de sistema
-
     noto-fonts-cjk-sans # Fontes Noto CJK sans
-
-   # mesa                             # Drivers Mesa padrão (Open Source)
-   # libva                            # Aceleração de vídeo VAAPI
-   # libvdpau                         # Suporte VDPAU (NVIDIA legacy, mas útil também com AMD via vaapiVdpau)
-   # vaapiVdpau                       # Ponte VAAPI ↔ VDPAU
-   # vulkan-loader                    # Biblioteca principal Vulkan
-   # vulkan-validation-layers         # Camadas de validação Vulkan (debug de jogos)
  
     libsForQt5.qt5ct                  # tema
     libsForQt5.breeze-icons 
     
-   # xdg-desktop-portal-gtk           # Portal GTK
-   # xdg-desktop-portal-wlr           # Portal WLR para Wayland 
+    # android-tools                  # adb
+    xdg-utils                        # steam para deligar
+    evemu                            # para deligar pela steam
+    wl-clipboard                     # copiar/colar 
+    xclip                            # copiar/colar
+  ];
 
-    cmake              # necessário para alguns builds que dependem dele
-    pkg-config         # para resolver dependências nativas
-    libdrm             # biblioteca de gerenciamento de recursos gráficos
-    wayland            # já que você está no Wayfire
-    meson              # sistema de build usado por wlroots
-    ninja              # backend do meson
-    wlroots
-    wayland-scanner
-    protontricks
-    icu # <- substitui libicu
-    glibc
-    zlib    # comum para descompressão
-    icu73
-    whitesur-icon-theme
-    liberation_ttf
-    nwg-look
-    wine
-    wineWowPackages.stable
-    winetricks
-    file
-    dotnetCorePackages.sdk_8_0_3xx
-    wtype
-    wayfirePlugins.wcm
-    libvirt
-    wl-clip-persist
-clipman
-wl-clipboard
-android-tools
-  fuse # AppImage
-vlc
+  programs.dconf.enable = true;      # Ativa o backend dconf no sistema para easyeffects
 
-
- gamescope
- gamemode
-
-    wl-clipboard # utilitário Wayland para clipboard
-    xclip         # utilitário X11 para clipboard (para o Proton)
-copyq
-];
-
-  hardware.graphics = {              # Configurações de GPU
+  hardware.graphics = {              # Configurações de GPU para steam
     enable = true;                   # Habilita drivers gráficos
     enable32Bit = true;   
     };
